9a239ff6ec755b0fdbcb175374c2b042ffbe5a15
Olivier Bussière <olivier2202@gmail.com>
2025-08-18 11:33:19 -0400

    Add intersection nodes and edge structures

--- tests/transportation_simulator_test.py
+++ tests/transportation_simulator_test.py
@@ -4,9 +4,10 @@ from src.simulation_core_model import SimulationCoreModel
 
 class TestTransportationSimulator:
     def test_config_network(self, simulator: TransportationSimulator):
-        assert simulator.graph.x.size(0) == 2
-        assert simulator.graph.edge_index.size(1) == 2
-        assert simulator.graph.edge_attr.size(0) == 2
+        assert simulator.graph.x.size(0) == 6
+        assert simulator.graph.edge_index.size(1) == 6
+        assert simulator.graph.edge_attr.size(0) == 6
+        assert simulator.graph.edge_index_routes.size(1) == 2
 
     def test_config_core(self, simulator: TransportationSimulator):
         assert isinstance(simulator.model_core, SimulationCoreModel)
